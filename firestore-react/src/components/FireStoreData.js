2ab90c784cd252107ae3f8fef631ec500407718f
jdeleonardis <jdeleonardis@hotmail.com>
2021-01-22 13:00:41 -0500

    Added code for adding and retrieving data

---
+++ firestore-react/src/components/FireStoreData.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useGetData } from "../hooks/useGetData";
+
+const FireStoreData = () => {
+  const [documents] = useGetData();
+
+
+  return (
+    <div>
+      <span>Values</span>
+      {documents.map((documents) => (
+        <div key={documents.id}>
+          <div>
+            Document: {documents.id} Value: {documents.value.value}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FireStoreData;
--- firestore-react/src/index.js
+++ firestore-react/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
